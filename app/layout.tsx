dbc6ac78e588c5aa4cb39237105965abf1c26ba2
Shakib Khan <leadwithshakib@gmail.com>
2026-01-08 13:58:21 +0600

    chore: update layout file for improved structure and organization

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  icons: {
+ icons: {
     icon: [
       {
         url: "/favicon_io/favicon-16x16.png",
@@ -66,16 +66,21 @@ export const metadata: Metadata = {
         sizes: "32x32",
         type: "image/png",
       },
-      { url: "/favicon_io/favicon.ico", sizes: "any" },
-    ],
-    apple: [{ url: "/favicon_io/apple-touch-icon.png" }],
-    other: [
+      { url: "/favicon_io/favicon.ico", sizes: "any", type: "image/x-icon" },
       {
-        rel: "manifest",
-        url: "/favicon_io/site.webmanifest",
+        url: "/favicon_io/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicon_io/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
+    apple: "/favicon_io/apple-touch-icon.png",
   },
+  manifest: "/favicon_io/site.webmanifest",
 };
 
 export default function RootLayout({
